32b732e37e52d5f7c9860907a34cbc8e626af0e1
DannyPreye <99923947+DannyPreye@users.noreply.github.com>
2022-08-14 18:53:33 +0100

    Finished the Services section

--- components/Featutes.jsx
+++ components/Featutes.jsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 
-export default ({ src }) => {
+export default ({ src, children }) => {
   return (
-    <div>
-      <div>
-        <Image src={src} alt="" />
+    <div className="flex flex-col items-center">
+      <div className=" w-[10rem] h-[10rem]">
+        <Image src={src} alt="" objectFit="cover" layout="intrinsic" />
+      </div>
+      <div className="flex flex-col gap-[1rem] items-center justify-center mt-2">
+        {children}
       </div>
     </div>
   );
--- components/Services.jsx
+++ components/Services.jsx
@@ -1,3 +1,8 @@
+import s1 from "../assets/s1.png";
+import s2 from "../assets/s2.png";
+import s3 from "../assets/s3.png";
+import Featutes from "./Featutes";
+
 export default () => {
   return (
     <section>
@@ -8,6 +13,27 @@ export default () => {
       </div>
 
       {/* Features */}
+      <div className="flex mt-[3rem] gap-[3rem] items-center justify-center">
+        <Featutes src={s1}>
+          <span className="font-bold">Easy to Order</span>
+          <span className="text-gray break-words w-[20rem] text-center">
+            You only need a few steps in food <br /> ordering
+          </span>
+        </Featutes>
+
+        <Featutes src={s2}>
+          <span className="font-bold">Easy to Order</span>
+          <span className="text-gray break-words w-[20rem] text-center">
+            Delivery that is always on time even faster
+          </span>
+        </Featutes>
+        <Featutes src={s3}>
+          <span className="font-bold">Easy to Order</span>
+          <span className="text-gray break-words w-[20rem] text-center">
+            Not only fast for us, quality is also number one
+          </span>
+        </Featutes>
+      </div>
     </section>
   );
 };
